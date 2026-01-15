9dcc034b5d877b064743ac252bd9c933d3ecfa18
Anne Lüken <anne.lkn@hotmail.com>
2026-01-15 13:43:55 +0100

    changed link to button

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,27 +1,26 @@
 import FooterNavigation from "@/components/FooterNavigation";
-import HomeNavigation from "@/components/HomeNavigation";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styled from "styled-components";
 import useSWR from "swr";
 
 export default function QuizPage() {
   const { data: prompts, isLoading, error } = useSWR("/api/prompts");
+  const router = useRouter();
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Failed to load data.</p>;
   if (!prompts) return null;
 
   function handleResetQuiz() {
+    router.push(`/quiz/${prompts[0]._id}`);
     localStorage.removeItem("quizAnswers");
   }
 
-  console.log(localStorage);
   return (
     <>
       <StyledWrapper>
-        <StyledLink href={`/quiz/${prompts[0]._id}`} onClick={handleResetQuiz}>
-          Play
-        </StyledLink>
+        <button onClick={handleResetQuiz}>Play</button>
       </StyledWrapper>
       <FooterNavigation />
     </>
@@ -35,6 +34,6 @@ const StyledLink = styled(Link)`
 const StyledWrapper = styled.div`
   display: grid;
   place-items: center;
-  height: 200px;
-  border: 3px solid green;
+  height: 100vh;
+  border: 3px solid black;
 `;
